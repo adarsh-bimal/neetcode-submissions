7f864d584ae05c5b8fc63219b098688c16a276c1
adarsh bimal <85839886+adarsh-bimal@users.noreply.github.com>
2026-09-18 18:20:29 +0530

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> temp = new Stack<>();
+    int min;
+    public MinStack() {
+        stack = new Stack<>();
+        temp = new Stack<>();
+    }
+    
+    public void push(int val) {
+         stack.push(val);
+         if(temp.isEmpty() || val <= temp.peek()){
+            temp.push(val);
+         }
+    }
+    
+    public void pop() {
+         if(stack.peek().equals(temp.peek())){
+            temp.pop();
+         }
+         stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+       return(temp.peek());
+        
+    }
+}
